8ac4d0cafe178050f71ba9a06644b15c2517d616
askarbek101 <askarbek101@gmail.com>
2025-02-11 10:05:46 -0800

    some changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: "public", // Ensure assets and index.html are detected
   build: {
     rollupOptions: {
       output: {
