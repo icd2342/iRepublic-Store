e9bc6c1480562a8f9e9cb98385cf1bbae17f77bb
askarbek101 <askarbek101@gmail.com>
2025-02-11 09:56:07 -0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Separate vendor libraries
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000 // Increase warning limit (optional)
+  }
 })
